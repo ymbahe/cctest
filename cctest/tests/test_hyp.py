9e87e59ed59ba7e55542333ee91c477affc88ab6
Yannick Bahe <bahe@strw.leidenuniv.nl>
2022-07-14 17:24:25 +0200

    Fixed deliberately failing test in test_hyp.py

--- cctest/tests/test_hyp.py
+++ cctest/tests/test_hyp.py
@@ -11,7 +11,7 @@ def encode(input_string):
             if prev:
                 entry = (prev, count)
                 lst.append(entry)
-            #count = 1
+            count = 1
             prev = character
         else:
             count += 1
